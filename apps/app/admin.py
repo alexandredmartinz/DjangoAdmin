d9934a9aa07e0f8485e68ed08dc7d37689474953
alexandredmartinz <alexandredmartinz@gmail.com>
2022-07-06 23:15:24 -0300

    Ajustes pasta media, registro modelos no admin

--- apps/app/admin.py
+++ apps/app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import Proprietario, Veiculo, Acessorio
+
 # Register your models here.
+
+admin.site.register(Proprietario)
+admin.site.register(Veiculo)
+admin.site.register(Acessorio)
